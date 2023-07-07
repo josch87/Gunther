3f0af40c42e2d6754bc28c3ff7257e7806d55350
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-07-07 15:50:12 +0200

    lifting up contactsState

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import GlobalStyle from "../styles";
+import { useImmerLocalStorageState } from "@/hooks/useImmerLocalStorageState";
+import { contactsSampleData } from "@/data/SampleData";
 
 export default function App({ Component, pageProps }) {
+  const [contacts, updateContacts] = useImmerLocalStorageState("contacts", {
+    defaultValue: contactsSampleData,
+  });
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} contacts={contacts} />
     </>
   );
 }
