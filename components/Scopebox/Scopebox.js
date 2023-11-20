3205164ecfcafb5b632d65a446a9b1fbd94a55d1
/Aljoscha <mail@aljoschazoeller.com>
2023-11-20 08:41:52 +0100

    extend scopebox with heading and add to dashboard

--- components/Scopebox/Scopebox.js
+++ components/Scopebox/Scopebox.js
@@ -1,5 +1,10 @@
-import { StyledScopebox } from "./Scopebox.styled";
+import { StyledHeading, StyledScopebox } from "./Scopebox.styled";
 
-export default function Scopebox({ children }) {
-  return <StyledScopebox>{children}</StyledScopebox>;
+export default function Scopebox({ children, heading }) {
+  return (
+    <>
+      {heading ? <StyledHeading>{heading}</StyledHeading> : null}
+      <StyledScopebox>{children}</StyledScopebox>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Heading from "@/components/Heading/Heading";
 import DefaultHead from "@/components/Layout/DefaultHead/DefaultHead";
+import Scopebox from "@/components/Scopebox/Scopebox";
 import Link from "next/link";
 
 export default function Dashboard() {
@@ -7,6 +8,8 @@ export default function Dashboard() {
     <>
       <DefaultHead pageTitle="Dashboard" />
       <Heading level={1}>Dashboard</Heading>
+
+      <Scopebox heading="Upcoming">Test</Scopebox>
     </>
   );
 }
