65004fae1791e92c96d07edb59a2743087758392
/Aljoscha <mail@aljoschazoeller.com>
2023-11-21 17:33:50 +0100

    refactor code and bring logic to utils functions

--- components/DashboardWidgets/UpcomingWidget/UpcomingWidget.js
+++ components/DashboardWidgets/UpcomingWidget/UpcomingWidget.js
@@ -1,33 +1,13 @@
 import InteractionListItem from "@/components/InteractionListItem/InteractionListItem";
 import Scopebox from "@/components/Scopebox/Scopebox";
 import { StyledUnorderedList } from "./UpcomingWidget.styled";
+import { getSortedActiveFutureInteractions } from "@/utils/getInteractionDetails";
 
 export default function UpcomingWidget({ interactions, contacts }) {
-  const activeInteractions = interactions.filter(
-    (interaction) =>
-      interaction.dateDeleted === null || interaction.dateDeleted === ""
+  const futureInteractionsToBeDisplayed = getSortedActiveFutureInteractions(
+    interactions,
+    3
   );
-
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-
-  const sortedFutureInteractions = activeInteractions
-    .filter((activeInteraction) => {
-      const interactionDate = new Date(activeInteraction.dateOfInteraction);
-      return interactionDate >= today;
-    })
-    .sort((a, b) => {
-      if (a.dateOfInteraction < b.dateOfInteraction) {
-        return -1;
-      }
-      if (a.dateOfInteraction > b.dateOfInteraction) {
-        return 1;
-      }
-
-      return 0;
-    });
-
-  const futureInteractionsToBeDisplayed = sortedFutureInteractions.slice(0, 3);
 
   return (
     <>
--- utils/getInteractionDetails.js
+++ utils/getInteractionDetails.js
@@ -14,3 +14,42 @@ export function getInteractionIcon({ type }) {
 export function getParticipant(contacts, contactId) {
   return contacts.find((contact) => contact.id === contactId);
 }
+
+export function getActiveInteractions(interactions) {
+  const activeInteractions = interactions.filter(
+    (interaction) =>
+      interaction.dateDeleted === null || interaction.dateDeleted === ""
+  );
+
+  return activeInteractions;
+}
+
+export function getSortedActiveFutureInteractions(interactions, count) {
+  const activeInteractions = getActiveInteractions(interactions);
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const sortedFutureInteractions = activeInteractions
+    .filter((activeInteraction) => {
+      const interactionDate = new Date(activeInteraction.dateOfInteraction);
+      return interactionDate >= today;
+    })
+    .sort((a, b) => {
+      if (a.dateOfInteraction < b.dateOfInteraction) {
+        return -1;
+      }
+      if (a.dateOfInteraction > b.dateOfInteraction) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+  const futureInteractionsToBeDisplayed = sortedFutureInteractions.slice(
+    0,
+    count
+  );
+
+  return futureInteractionsToBeDisplayed;
+}
