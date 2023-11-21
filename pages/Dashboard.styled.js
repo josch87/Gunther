c34be965d4dcfc6df7e688a919179a67fdc8fdbe
/Aljoscha <mail@aljoschazoeller.com>
2023-11-21 17:59:08 +0100

    add note to dashboard to start with contacts and interactions

---
+++ pages/Dashboard.styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyledParagraph = styled.p`
+  line-height: 1.5rem;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
 import UpcomingWidget from "@/components/DashboardWidgets/UpcomingWidget/UpcomingWidget";
 import Heading from "@/components/Heading/Heading";
 import DefaultHead from "@/components/Layout/DefaultHead/DefaultHead";
+import { getSortedActiveFutureInteractions } from "@/utils/getInteractionDetails";
+import Link from "next/link";
+import { StyledParagraph } from "./Dashboard.styled";
 
 export default function Dashboard({ interactions, contacts }) {
+  const futureInteractionsToBeDisplayed = getSortedActiveFutureInteractions(
+    interactions,
+    3
+  );
+
   return (
     <>
       <DefaultHead pageTitle="Dashboard" />
       <Heading level={1}>Dashboard</Heading>
+      {futureInteractionsToBeDisplayed.length === 0 ? (
+        <>
+          <StyledParagraph>
+            Start by adding some <Link href="/contacts">contacts</Link> and{" "}
+            <Link href="/interactions">interactions</Link>.
+          </StyledParagraph>
+        </>
+      ) : null}
       <UpcomingWidget interactions={interactions} contacts={contacts} />
     </>
   );
