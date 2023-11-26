1475f6d7ab338e01d22d5e6640fba37c21741725
/Aljoscha <mail@aljoschazoeller.com>
2023-11-26 11:22:01 +0100

    move welcomeText to WelcomeMessageWidget component

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,14 @@
 import UpcomingWidget from "@/components/DashboardWidgets/UpcomingWidget/UpcomingWidget";
+import WelcomeMessageWidget from "@/components/DashboardWidgets/WelcomeMessageWidget/WelcomeMessageWidget";
 import Heading from "@/components/Heading/Heading";
 import DefaultHead from "@/components/Layout/DefaultHead/DefaultHead";
 import { getSortedActiveFutureInteractions } from "@/utils/getInteractionDetails";
-import Link from "next/link";
 import styled from "styled-components";
-
-const StyledParagraph = styled.p`
-  line-height: 1.5rem;
-`;
 
 const FlexContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1.5rem;
-`;
-
-const StyledLink = styled(Link)`
-  color: var(--primary-text-color);
-  text-decoration-color: var(--primary-color);
-  text-underline-offset: 2px;
-
-  &:active {
-    background-color: var(--primary-color);
-  }
 `;
 
 export default function Dashboard({ interactions, contacts }) {
@@ -34,15 +20,10 @@ export default function Dashboard({ interactions, contacts }) {
   return (
     <>
       <DefaultHead pageTitle="Dashboard" />
+      <Heading level={1}>Dashboard</Heading>
       <FlexContainer>
         <section>
-          <Heading level={1}>Dashboard</Heading>
-          <StyledParagraph>
-            Welcome to Gunther - your Personal Relationship Management app.
-            Manage your <StyledLink href="/contacts">contacts</StyledLink> and
-            social <StyledLink href="/interactions">interactions</StyledLink>{" "}
-            and cultivate a fulfilling network!
-          </StyledParagraph>
+          <WelcomeMessageWidget />
         </section>
         {futureInteractionsToBeDisplayed.length > 0 ? (
           <section>
