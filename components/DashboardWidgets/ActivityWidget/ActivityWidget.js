c75fb65e11835cb863a3e576b491e87ed18e14db
/Aljoscha <mail@aljoschazoeller.com>
2023-12-10 16:43:09 +0100

    add StyledUnorderedList to activityWdiget

--- components/DashboardWidgets/ActivityWidget/ActivityWidget.js
+++ components/DashboardWidgets/ActivityWidget/ActivityWidget.js
@@ -1,29 +1,33 @@
 import ActivityListItem from "@/components/ActivityListItem/ActivityListItem";
 import Scopebox from "@/components/Scopebox/Scopebox";
+import { StyledUnorderedList } from "./ActivityWidget.styled";
 
 export default function ActivityWidget({ activities, contacts, interactions }) {
   return (
     <Scopebox heading="Activities">
-      {activities.map((activity) => {
-        const contact = contacts.find(
-          (contact) =>
-            contact.id === activity.newData.id && activity.entity === "Contact"
-        );
-        const interaction = interactions.find(
-          (interaction) =>
-            interaction.id === activity.newData.id &&
-            activity.entity === "Interaction"
-        );
+      <StyledUnorderedList>
+        {activities.map((activity) => {
+          const contact = contacts.find(
+            (contact) =>
+              contact.id === activity.newData.id &&
+              activity.entity === "Contact"
+          );
+          const interaction = interactions.find(
+            (interaction) =>
+              interaction.id === activity.newData.id &&
+              activity.entity === "Interaction"
+          );
 
-        return (
-          <ActivityListItem
-            key={activity.id}
-            activity={activity}
-            contact={contact}
-            interaction={interaction}
-          />
-        );
-      })}
+          return (
+            <ActivityListItem
+              key={activity.id}
+              activity={activity}
+              contact={contact}
+              interaction={interaction}
+            />
+          );
+        })}
+      </StyledUnorderedList>
     </Scopebox>
   );
 }
