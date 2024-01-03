73c47fd4a3362e156a78ef6536d707eec6205182
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-01-03 23:17:24 +0100

    upgrade next, test environment

--- components/ContactListItem/ContactListItem.js
+++ components/ContactListItem/ContactListItem.js
@@ -10,8 +10,20 @@ import {
 import { materialImage } from "@/assets/Icons8";
 
 export default function ContactListItem({ contact }) {
+  if (process.env.NODE_ENV !== "production") {
+    console.warn(
+      "DEPRECATED: ContactListItem is no longer recommended for use."
+    );
+  }
+
+  console.log("NODE_EVN:", process.env.NODE_ENV);
+  console.log("env:", process.env.NEXT_PUBLIC_ENVIRONMENT);
+
   return (
     <ListItem>
+      {process.env.NODE_ENV}
+      <br />
+      {process.env.NEXT_PUBLIC_ENVIRONMENT}
       <StyledLink href={`/contacts/${contact.id}`} title="Show contact details">
         {contact.profilePicture ? (
           <ProfileImage
@@ -26,7 +38,6 @@ export default function ContactListItem({ contact }) {
             width={80}
             height={80}
             alt={`Placeholder for profile picture of ${getFullName(contact)}`}
-            placeholder={true}
           />
         )}
         <DetailsContainer>
